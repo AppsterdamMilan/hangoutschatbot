e6860064a9375baa82bf9eb0bf60d490230feaf3
Gianfranco Reppucci <gianfranco.reppucci@gmail.com>
2018-10-08 18:14:56 +0200

    add basic structure

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+var (
+	googleCredentialsFilePath = flag.String("google-credentials", "./credentials.json", "path to credentials file (default './credentials.json')")
+	hangoutsServerPort        = flag.Int("hangouts-server-port", 8080, "port to run the Hangouts HTTP server on (default 8080)")
+)
+
+func main() {
+	flag.Parse()
+
+	// set google credentials via environment variable using file
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", *googleCredentialsFilePath)
+
+	// initialize chat service and register plugins
+	cs, err := NewChatService(*hangoutsServerPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// start chat service
+	log.Fatal(cs.Serve())
+}
